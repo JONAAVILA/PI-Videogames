91a3f2ba797b1c8c9126c10773e63ddefe6dc148
JONAAVILA <jona_1969@hotmail.com>
2024-01-24 23:56:40 -0300

    'model videogames complete'

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -8,5 +8,28 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    platform: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        isDate: true
+      }
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull:false
+    }
   });
 };
