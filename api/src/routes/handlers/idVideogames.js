9ee226ee1ecccd821daf5be66455fedc3f771c4a
JONAAVILA <jona_1969@hotmail.com>
2024-02-06 23:42:15 -0300

    'fixed db'

--- api/src/routes/handlers/idVideogames.js
+++ api/src/routes/handlers/idVideogames.js
@@ -3,13 +3,13 @@ const axios = require('axios');
 const { APY_KEY } = process.env;
 
 const idVideogames = async (idGame)=>{
-    if(idGame > 20){
-        const game = await axios(`https://api.rawg.io/api/games/${idGame}?key=${APY_KEY}`)
-        return game.data
-    }
-    const game = await Videogame.findByPk(idGame)
-    if(!game) throw new Error('Game not found')
-    return game
+    
+    const game = await axios(`https://api.rawg.io/api/games/${idGame}?key=${APY_KEY}`)
+    
+    return game.data
+    
+
+    
 }
 
 module.exports = idVideogames;
