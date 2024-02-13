3eb02ad89822ee00e6c0e5c0f31e3b7b5802df3b
JONAAVILA <jona_1969@hotmail.com>
2024-02-13 11:13:35 -0300

    resolve get all games

--- api/src/routes/controllers/getAllGenres.js
+++ api/src/routes/controllers/getAllGenres.js
@@ -3,7 +3,6 @@ const allGenres = require('./../handlers/allGenres');
 const getAllGenres = async (req, res)=>{
     try {
         const genres = await allGenres()
-        console.log(genres)
         return res.status(200).json(genres)
     } catch (error) {
         return res.status(400).json({error:error.message})
--- api/src/routes/handlers/allVideogames.js
+++ api/src/routes/handlers/allVideogames.js
@@ -1,9 +1,11 @@
 const { Videogame } = require('../../db');
+const axios = require('axios');
+const { API_KEY } = process.env;
+const URL = 'https://api.rawg.io/api/games?';
 
 const allVideoGames = async ()=>{
-        const allGames = await Videogame.findAll()
-        if(!allGames) throw new Error({error: 'Videogames not found'})
-        return allGames
+        const allGames  = await axios(`${URL}key=${API_KEY}`)
+        
 }
 
 module.exports = allVideoGames;
