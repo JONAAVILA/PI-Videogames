365fc496e020250937b622ae82b448b3e962c0b2
JONAAVILA <jona_1969@hotmail.com>
2024-02-14 23:46:41 -0300

    fixed get all games

--- api/src/routes/controllers/getAllGames.js
+++ api/src/routes/controllers/getAllGames.js
@@ -2,7 +2,9 @@ const allVideoGames = require('../handlers/allVideogames');
 
 const getAllVideogames = async (req, res)=>{
     try {
-        const videoGames = await allVideoGames()
+        const { page } = req.query
+        console.log(page)
+        const videoGames = await allVideoGames(page)
         res.status(200).json(videoGames)
     } catch (error) {
         res.status(400).json({error:error.message})
--- api/src/routes/handlers/allVideogames.js
+++ api/src/routes/handlers/allVideogames.js
@@ -3,9 +3,13 @@ const axios = require('axios');
 const { API_KEY } = process.env;
 const URL = 'https://api.rawg.io/api/games?';
 
-const allVideoGames = async ()=>{
-        const allGames  = await axios(`${URL}key=${API_KEY}`)
-        
+const allVideoGames = async (page)=>{
+        if(page === 1){
+                const allGames = await axios(`${URL}key=${API_KEY}`)
+                return allGames.data.results
+        }
+        const allGames = await axios(`${URL}key=${API_KEY}&page=${page}`)
+        return allGames.data.results
 }
 
 module.exports = allVideoGames;
